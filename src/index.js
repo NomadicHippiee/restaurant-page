85f8c5387b2a36e79b34917ef77f55d6dac47661
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-01-08 00:07:41 +0100

    Tab navigation function with eventListners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 import "./styles.css";
 import homePage from "../pages/home.js";
+import menuPage from "../pages/menu.js";
 
-homePage();
+function tabNavigation() {
+    const homeTab = document.querySelector("#home");
+    const menuTab = document.querySelector("#menu");
+    const contactTab = document.querySelector("#contact");
+
+    homeTab.addEventListener("click", () => {
+        homePage();
+        setActiveTab(homeTab);
+    });
+
+    menuTab.addEventListener("click", () => {
+        menuPage();
+        setActiveTab(menuTab);
+    });
+
+    contactTab.addEventListener("click", () =>{
+        contactPage();
+        setActiveTab(contactTab);
+    });
+
+    homePage();
+    setActiveTab(homeTab);
+}
+
+function setActiveTab(activeButton) {
+    const buttons = document.querySelectorAll("nav button");
+    buttons.forEach(btn => btn.classList.remove("active"));
+    activeButton.classList.add("active");
+
+}
+
+tabNavigation();
+
+
